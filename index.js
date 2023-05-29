5f9e2edfe16ac83593a042f7bfaa55ea8cc59819
Richard <cardozo.nov@gmail.com>
2023-05-29 14:56:44 -0500

    codes in js

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 console.log(document);
+
+const heading = document.querySelector("h1");
+console.log(heading);
+
+const value = document.querySelector(".banner");
+console.log(value);
+
+const button = document.querySelector("button");
+console.log(button);
+
+const area = document.querySelector(".area-display");
+console.log(area);
+
+const div = document.querySelector("div .stat");
+console.log(div);
+
+const hello = document.querySelector("hello");
+console.log(hello);
+
+const buttons = document.querySelectorAll("button");
+console.log(buttons);
+
+const heading3List = document.querySelectorAll("h3");
+
+// Iterate over the list and print each one
+for (let element of heading3List.values()) {
+  console.log(element);
+}
+
+const divRating = document.querySelectorAll("div .rating-display") 
+
+for (let divRate of divRating.values()) {
+    console.log(divRate);
+}
+
+const divArea = document.querySelectorAll("div .area-display")
+for (let i = 0; i < divArea.length; i++) {
+    const divElement = divArea[i];
+    console.log(divElement);
+  }
