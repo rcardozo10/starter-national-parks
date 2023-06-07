072d3809fda0f26aa7281be1a00e02157c482b49
Richard Cardozo <127159026+rcardozo10@users.noreply.github.com>
2023-06-07 15:16:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,224 +1,157 @@
-/*
-console.log(document);
+const submitHandler = (event) => {
+  event.preventDefault();
 
-const heading = document.querySelector("h1");
-console.log(heading);
+  const form = document.querySelector("#park-form");
+  const formData = new FormData(form);
+  // Keep track of if any errors are found
+  let hasErrors = false;
 
-const value = document.querySelector(".banner");
-console.log(value);
-
-const button = document.querySelector("button");
-console.log(button);
-
-const area = document.querySelector(".area-display");
-console.log(area);
-
-const div = document.querySelector("div .stat");
-console.log(div);
-
-const hello = document.querySelector("hello");
-console.log(hello);
-
-const buttons = document.querySelectorAll("button");
-console.log(buttons);
-
-
-const heading3List = document.querySelectorAll("h3");
-// Iterate over the list and print each one
-for (let element of heading3List.values()) {
-  console.log(element);
-}
-
-
-//  using let of to array over object
-const divRating = document.querySelectorAll("div .rating-display") 
-for (let divRate of divRating.values()) {
-    console.log(divRate);
-}
-
-//  using for loop to array over object
-const divArea = document.querySelectorAll("div .area-display")
-for (let i = 0; i < divArea.length; i++) {
-    const divElement = divArea[i];
-    console.log(divElement);
-  }
-*/
-
-/*
-const descriptions = document.querySelectorAll(".description-display");
-for (let desc of descriptions.values()) {
-    let content = desc.innerText;
-    if(content.length > 250) {
-        content = content.slice(0, 250);
-        content = content + "...";
-    }
-    console.log(content);
-  }
-  */
-
-  const descriptions = document.querySelectorAll(".description-display");
-  /*
-  for (let desc of descriptions.values()) {
-    let content = desc.innerText;
-  
-    if (content.length > 250) {
-      content = content.slice(0, 250);
-      content = content + "...";
-    }
-  
-    desc.innerText = content;
-  } */
-/*
-  for (let desc of descriptions.values()) {
-    let content = desc.innerText;
-  
-    if (content.length > 250) {
-      content = content.slice(0, 250);
-      content = content + '<a href="#">...</a>';
-    }
-  
-    desc.innerHTML = content;
-  }
-  */
-  const ratings = document.querySelectorAll(".rating-display .value");
-  /*
-  for (let rating of ratings) {
-    let ratingValue = parseFloat(rating.innerText);
-    if (ratingValue > 4.7) {
-        rating.style.fontWeight = "bold";
-      }
-    console.log(ratingValue);
-  }*/
-  for (let rating of ratings) {
-    let ratingValue = parseFloat(rating.innerText);
-  
-    if (ratingValue > 4.7) {
-      rating.classList.add("high-rating");
-      rating.classList.remove("value");
-    }
-  }
-
-  const parks = document.querySelectorAll(".park-display");
-  const numberParks = parks.length;
-  const newElement = document.createElement("div");
-  newElement.innerText = `${numberParks} exciting parks to visit`;
-  newElement.classList.add("header-statement");
-  const header = document.querySelector("header");
-  header.appendChild(newElement);
-
-  // Get the parent element of all parks
-const main = document.querySelector("main");
-
-// Select a single park
-const park = main.querySelector(".park-display");
-
-// Remove that park
-main.removeChild(park);
-
-const firstBtn = document.querySelector("button");
-firstBtn.addEventListener("click", (event) => {
-    console.log("You clicked the button", event);
-  });
-
-  // Select the `nameSorter` link
-const nameSorter = document.querySelector("#name-sorter");
-
-// Add an event listener
-nameSorter.addEventListener("click", (event) => {
-  console.log("You clicked the name sorter");
-});
-
-nameSorter.addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log("You clicked the name sorter");
-  });
-
-  nameSorter.addEventListener("click", (event) => {
-    event.preventDefault();
-  
-    // 1.  Get the main element
-    const main = document.querySelector("main");
-  
-    // 2. Get the list of parks
-    const parksList = main.querySelectorAll(".park-display");
-  
-    // 3. Empty the main element
-    main.innerHTML = "";
-  });
-
-  // 4. Create an array
-const parksArray = Array.from(parksList);
-
-// 5. Sort the array
-parksArray.sort((parkA, parkB) => {
-    const parkAName = parkA.querySelector("h2").innerText;
-    const parkBName = parkB.querySelector("h2").innerText;
-    if (parkAName < parkBName) {
-      return -1;
-    } else if (parkAName > parkBName) {
-      return 1;
+  formData.forEach((value, key) => {
+    let errorId = `#${key}-error`;
+    if (value.trim() === "") {
+      document.querySelector(errorId).style.display = "block";
+      hasErrors = true;
     } else {
-      return 0;
+      document.querySelector(errorId).style.display = "none";
     }
   });
 
-  // 6. Insert each park into the DOM
-  parksArray.forEach((park) => {
-    main.appendChild(park);
-  });
+  // if there are no errors
+  if (!hasErrors) {
+    // create an empty object
+    const park = {
+      name: formData.get("name"),
+      location: formData.get("location"),
+      description: formData.get("description"),
+      established: formData.get("established"),
+      area: formData.get("area"),
+      rating: formData.get("rating"),
+    };
 
+    parks.push(park);
 
-/*
-  // Function for sorting by name
+    render();
+  }
+};
+
+// function to handler favorite button clicks
+const favoriteButtonClickHandler = (event) => {
+  if (event.target && event.target.nodeName == "BUTTON") {
+    const park = event.target.parentNode;
+    park.style.backgroundColor = "#c8e6c9";
+  }
+};
+
+// function for sorting by name
 const sortByName = (parkA, parkB) => {
-    const parkAName = parkA.querySelector("h2").innerText;
-    const parkBName = parkB.querySelector("h2").innerText;
-    if (parkAName < parkBName) {
-      return -1;
-    } else if (parkAName > parkBName) {
-      return 1;
-    } else {
-      return 0;
-    }
-  };
-  
-  // Function for handling the `nameSorter` click
-  const nameSorterClickHandler = (event) => {
-    event.preventDefault();
-  
-    // 1.  Get the main element
-    const main = document.querySelector("main");
-  
-    // 2. Get the list of parks
-    const parksList = main.querySelectorAll(".park-display");
-  
-    // 3. Empty the main
-    main.innerHTML = "";
-  
-    // 4. Create an array
-    const parksArray = Array.from(parksList);
-  
-    // 5. Sort the array
-    parksArray.sort(sortByName);
-  
-    // 6. Insert each park into the DOM
-    parksArray.forEach((park) => {
-      main.appendChild(park);
-    });
-  };
-  
-  // Select the `nameSorter` link
+  const parkAName = parkA.name;
+  const parkBName = parkB.name;
+  if (parkAName < parkBName) {
+    return -1;
+  } else if (parkAName > parkBName) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+// function for sorting by rating
+const sortByRating = (parkA, parkB) => {
+  const parkARating = parseFloat(parkA.rating);
+  const parkBRating = parseFloat(parkB.rating);
+  return parkBRating - parkARating;
+};
+
+// function for handling the nameSorter click
+const nameSorterClickHandler = (event) => {
+  event.preventDefault();
+
+  parks.sort(sortByName);
+
+  render();
+};
+
+// function to handle the ratingSorter click
+const ratingSorterClickHandler = (event) => {
+  event.preventDefault();
+
+  parks.sort(sortByRating);
+
+  render();
+};
+
+const renderOnePark = (park) => {
+  // get the individual properties of the park
+  const { name, location, description, established, area, rating } = park;
+
+  const content = `
+      <section class="park-display">
+        <h2>${name}</h2>
+        <div class="location-display">${location}</div>
+        <div class="description-display">${description}</div>
+        <button class="rate-button" title="Add to Favourites">&#9734;</button>
+        <div class="stats">
+          <div class="established-display stat">
+            <h3>Established</h3>
+            <div class="value">${established}</div>
+          </div>
+          <div class="area-display stat">
+            <h3>Area</h3>
+            <div class="value">${area}</div>
+          </div>
+          <div class="rating-display stat">
+            <h3>Rating</h3>
+            <div class="value">${rating}</div>
+          </div>
+        </div>
+      </section>
+  `;
+  return content;
+};
+
+const render = () => {
+  // get the parent element
+  const main = document.querySelector("main");
+
+  // 1. empty the parent element
+  main.innerHTML = "";
+
+  // 2. get the parks HTML
+  const content = parks.map(renderOnePark).join("");
+
+  // 3. Set innerHTML of parent element
+  main.innerHTML = content;
+};
+
+// the point where all the code starts
+const main = () => {
+  // select the nameSorter link
   const nameSorter = document.querySelector("#name-sorter");
-  
-  // Add an event listener
+
+  // add an event listener
   nameSorter.addEventListener("click", nameSorterClickHandler);
 
-  console.log("Before!");
+  // select the ratingSorter link
+  const ratingSorter = document.querySelector("#rating-sorter");
 
-window.addEventListener("DOMContentLoaded", (event) => {
-  console.log("Loaded!");
-});
+  // add an event listener
+  ratingSorter.addEventListener("click", ratingSorterClickHandler);
 
+  // select all the buttons for all the parks
+  const main = document.querySelector("main");
 
+  // add event handler to the main
+  main.addEventListener("click", favoriteButtonClickHandler);
 
-*/
+  // get the form element
+  const form = document.querySelector("#park-form");
+
+  // attach the submit handler
+  form.addEventListener("submit", submitHandler);
+
+  // call render()
+  render();
+};
+
+// Add event listener for DOMContentLoaded
+window.addEventListener("DOMContentLoaded", main);
